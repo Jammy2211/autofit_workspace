ec7df0c8f811d20ca2a5b2d62a5b871e723457f2
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-12-23 12:55:01 +0000

    23/12/23

--- scripts/features/interpolate.py
+++ scripts/features/interpolate.py
@@ -2,19 +2,20 @@
 Feature: Interpolate
 ====================
 
-On may have multiple datasets where it is anticipated that one or more parameters vary smoothly across the datasets.
+It is common to fit a model to many similar datasets, where it is anticipated that one or more model parameters vary
+smoothly across the datasets.
 
 For example, the datasets may be taken at different times, where the signal in the data and therefore model parameters
-one infers vary smoothly as a function of time. Alternaitvely, the datasets may be taken at different wavelengths,
-with the signal varying smoothly as a function of wavelength.
+vary smoothly as a function of time. Alternaitvely, the datasets may be taken at different wavelengths, with the signal
+varying smoothly as a function of wavelength.
 
-In any of these cases, it could be desireable to fit the datasets one-by-one, and then interpolate the results in order
+In any of these cases, it may be desireable to fit the datasets one-by-one and then interpolate the results in order
 to determine the most likely model parameters at any point in time (or at any wavelength).
 
-This example illustrates interpolate functionality in **PyAutoFit** using the example of fitting 3 noisy 1D Gaussians,
-where these data are assumed to have been taken at 3 different times. The `centre` of each `Gaussian` varies smoothly
-over time. The interpolation is therefore used to estimate the `centre` of each `Gaussian` at any time between the
-three times the data was taken.
+This example illustrates model interpolation functionality in **PyAutoFit** using the example of fitting 3 noisy
+1D Gaussians, where these data are assumed to have been taken at 3 different times. The `centre` of each `Gaussian`
+varies smoothly over time. The interpolation is therefore used to estimate the `centre` of each `Gaussian` at any time
+outside of the times the data were observed.
 
 __Example Source Code (`af.ex`)__
 
@@ -42,8 +43,8 @@ import autofit as af
 """
 __Dataset__
 
-We load 3 noisy 1D Gaussian datasets taken at 3 different times, where the `centre` of each `Gaussian` varies smoothly 
-over time.
+We illustrate model interpolation using 3 noisy 1D Gaussian datasets taken at 3 different times, where the `centre` of 
+each `Gaussian` varies smoothly over time.
 
 The datasets are taken at 3 times, t=0, t=1 and t=2, which defines the name of the folder we load the data from.
 
@@ -57,12 +58,10 @@ time_list = []
 
 for time in range(3):
     dataset_name = f"time_{time}"
-    
+
     dataset_prefix_path = path.join("dataset", "example_1d", "gaussian_x1_variable")
 
-    dataset_path = path.join(
-        dataset_prefix_path, dataset_name
-    )
+    dataset_path = path.join(dataset_prefix_path, dataset_name)
 
     data = af.util.numpy_array_from_json(file_path=path.join(dataset_path, "data.json"))
     noise_map = af.util.numpy_array_from_json(
@@ -74,13 +73,10 @@ for time in range(3):
     time_list.append(time)
 
 """
-Now lets plot the data, including their error bars.
-
 Visual comparison of the datasets shows that the `centre` of each `Gaussian` varies smoothly over time, with it moving
 from pixel 40 at t=0 to pixel 60 at t=2.
 """
 for time in range(3):
-    
     xvalues = range(data_list[time].shape[0])
 
     plt.errorbar(
@@ -100,20 +96,20 @@ for time in range(3):
 
 
 """
+__Fit__
+
 We now fit each of the 3 datasets.
 
 The fits are performed in a for loop, with the docstrings inside the loop explaining the code.
 
-The interpolate at the end of the fits uses the maximum log likelihood model of each fit, which we therefore store 
-in a list.
+The interpolate at the end of the fits uses the maximum log likelihood model of each fit, which we store in a list.
 """
 ml_instances_list = []
 
 for data, noise_map, time in zip(data_list, noise_map_list, time_list):
-
     """
     __Analysis__
-    
+
     For each dataset we create an `Analysis` class, which includes the `log_likelihood_function` we fit the data with.
     """
     analysis = af.ex.Analysis(data=data, noise_map=noise_map)
@@ -121,11 +117,11 @@ for data, noise_map, time in zip(data_list, noise_map_list, time_list):
     """
     __Time__
     
-    The model composed below has an input not seen in other scripts, the parameter `time`.
+    The model composed below has an input not seen in other examples, the parameter `time`.
     
-    This is the time that the simulated data was acquired, and is not a free parameter in the fit. 
+    This is the time that the simulated data was acquired and is not a free parameter in the fit. 
     
-    For interpolation it plays a crucial role, as the model is interpolated to the time of every dataset as input
+    For interpolation it plays a crucial role, as the model is interpolated to the time of every dataset input
     into the model below. If the `time` input were missing, interpolation could not be performed.
     
     Over the iterations of the for loop, the `time` input will therefore be the values 0.0, 1.0 and 2.0.
@@ -138,10 +134,7 @@ for data, noise_map, time in zip(data_list, noise_map_list, time_list):
     
     We expect the inferred `centre` inferred from the fit to each dataset to vary smoothly as a function of time.
     """
-    model = af.Collection(
-        gaussian=af.Model(af.ex.Gaussian),
-        time=time
-    )
+    model = af.Collection(gaussian=af.Model(af.ex.Gaussian), time=time)
 
     """
     __Search__
@@ -225,12 +218,13 @@ interpolator = from_json(file_path=json_file)
 """
 __Database__
 
-It may be inconvenient to fit all the models in a single Python scfript, especially if the model-fits take a long time
-and you are fitting many datasets.
+It may be inconvenient to fit all the models in a single Python script (e.g. the model-fits take a long time and you 
+are fitting many datasets).
 
-PyAutoFit's database tools allow you to store the results of model-fits from hard-disk. 
+PyAutoFit's allows you to store the results of model-fits from hard-disk. 
 
-Below, we use these tools to load the results of the fit above, set up the interpolator and perform the interpolation.
+Database functionality then allows you to load the results of the fit above, set up the interpolator and perform the 
+interpolation.
 
 If you are not familiar with the database API, you should checkout the `cookbook/database.ipynb` example.
 """
@@ -251,3 +245,6 @@ print(f"Gaussian centre of fit 2 (t = 2): {ml_instances_list[1].gaussian.centre}
 
 print(f"Gaussian centre interpolated at t = 1.5 {instance.gaussian.centre}")
 
+"""
+Finish.
+"""
